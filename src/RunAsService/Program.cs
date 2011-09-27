6021735216b58d4feda61e03ef55c3283533c164
executor@comparis.ch <lukas.angerer@gmail.com>
2011-09-27 08:57:24 +0200

    - initial commit   - rudimentary service implementation with log4net logging

---
+++ src/RunAsService/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ServiceProcess;
+
+namespace RunAsService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun = null;
+
+            try
+            {
+                ServicesToRun = new ServiceBase[]
+                                    {
+                                        new RunAsService()
+                                    };
+            }
+            catch (Exception e)
+            {
+                var sysroot = System.IO.Path.GetPathRoot(Environment.SystemDirectory);
+                System.IO.File.AppendAllText(System.IO.Path.Combine(sysroot, "runasservice-crash.log"), e.ToString());
+                Environment.Exit(-1);
+            }
+
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ src/RunAsService/RunAsService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.ServiceProcess;
+using log4net;
+
+namespace RunAsService
+{
+    public partial class RunAsService : ServiceBase
+    {
+        private readonly ILog _log;
+
+        public RunAsService()
+        {
+            InitializeComponent();
+
+            var serviceDirectory = (new Uri(System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase))).AbsolutePath;
+            Environment.SetEnvironmentVariable("SERVICE_DIRECTORY", serviceDirectory);
+            log4net.Config.XmlConfigurator.Configure();
+
+            _log = LogManager.GetLogger(typeof (RunAsService));
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _log.Info("Service starting");
+        }
+
+        protected override void OnStop()
+        {
+            _log.Info("Service stopping");
+        }
+    }
+}
