1451a3715bf34959858c494f2abb10e6ddbb887f
executor@comparis.ch <lukas.angerer@gmail.com>
2011-09-29 16:48:04 +0200

    - added test application (daemon style console app)

---
+++ src/TestApplication/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace TestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting application");
+
+            Console.TreatControlCAsInput = false;
+            Process.GetCurrentProcess().Exited += ProgramExited;
+
+            int i = 0;
+            while (true)
+            {
+                Console.WriteLine("Doing service-y stuff...");
+                if (i % 13 == 0)
+                    Console.Error.WriteLine("There might be the occasional error");
+
+                Thread.Sleep(5000);
+                i++;
+            }
+        }
+
+        static void ProgramExited(object sender, EventArgs e)
+        {
+            Console.WriteLine("Stopping application");
+        }
+    }
+}
